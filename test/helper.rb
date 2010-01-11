6ae4afa1f5c66d2b499d3fc7e605d1fd51301f7a
ffu <ffujimurah@gmail.com>
2010-01-11 13:21:36 +0900

    Initial commit to speak_your_mind.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'speak_your_mind'
+
+class Test::Unit::TestCase
+end
